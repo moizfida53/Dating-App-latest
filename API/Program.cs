524f82cb926b3cebf2a46ea5f1ef407f424524a1
moizfida <moiztahafida@gmail.com>
2025-09-17 10:52:03 +0300

    end of section 2

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
@@ -13,5 +14,10 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 app.MapControllers();
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.Run();
---
+++ client/src/app/app.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = signal('client');
+
+  protected members = signal<any>("");
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/Members/").subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete:() => console.log("completed the http request")
+      
+    })
+  }
+}
